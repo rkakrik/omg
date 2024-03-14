a0e42bd064180309f0cf7b943290c24fdb15e56c
Kirill Fries-Raevskiy <exocarnivore@gmail.com>
2024-03-14 14:42:56 +0500

    Изменение языка на русский и добавление навигационной панели

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import tabler from 'yesvelte/css/tabler.min.css?url';
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href={tabler} />
+</svelte:head>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Navbar, NavbarItem } from 'yesvelte'
+</script>
+
+<Navbar theme="dark">
+	<NavbarItem icon="home" title="Home" />
+	<NavbarItem icon="star" title="Features" />
+	<NavbarItem icon="currency-dollar" title="Pricing" />
+	<NavbarItem title="Disabled" disabled />
+
+	<NavbarItem title="Dropdown link">
+		<NavbarItem title="Action" />
+		<NavbarItem icon="home" title="Another action" />
+		<NavbarItem icon="currency-dollar" title="Something else here" />
+	</NavbarItem>
+</Navbar>
